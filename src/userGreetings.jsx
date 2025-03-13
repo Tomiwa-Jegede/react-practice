b057c8fb41adf32a8a6bf0435f4ec6901e18ac6e
Tomiwa Jegede <jegedetomiwa1@gmail.com>
2025-03-13 22:21:41 +0100

    Update userGreetings

--- src/userGreetings.jsx
+++ src/userGreetings.jsx
@@ -2,11 +2,11 @@ import PropTypes from "prop-types";
 function UserGreetings(props)
 {
   const welcomeMessage = <h1>Welcome {props.userName}</h1>;
-  const notLocggedInMessage = <p>Pls login</p>;
-  return props.isLoggedIn ? welcomeMessage : notLocggedInMessage;
+  const notLoggedInMessage = <p>Pls login</p>;
+  return props.isLoggedIn ? welcomeMessage : notLoggedInMessage;
 };
 
-UserGreetings.propType = {
+UserGreetings.propTypes = {
   isLoggedIn: PropTypes.bool,
   userName: PropTypes.string,
 };
